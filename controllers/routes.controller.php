6cfc52c8412305463bb2aac3c608d3d95e6b7f41
manuelmsni <manuelmsni@hotmail.com>
2023-06-06 13:31:07 +0200

    first commit

---
+++ controllers/routes.controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class RoutesController{
+
+    /*
+     * Ruta principal  
+     * * * * * * * * */
+    public function index(){
+        include 'routes/routes.php';
+    }
+
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+     * Mostrar errores  
+     * * * * * * * * * */
+
+    ini_set('display_errors', 1);
+    ini_set('log_errors', 1);
+    ini_set('error_log', "C:/xampp/htdocs/resAPI/php_error_log.txt");
+
+    /*
+     * Requerimientos  
+     * * * * * * * * * */
+
+    require_once 'controllers/routes.controller.php';
+
+    $index= new RoutesController();
+
+    $index -> index();
+
+?>
