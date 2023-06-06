af5d428ae90f4d5fb0ad488e49f08a784dde0888
manuelmsni <manuelmsni@hotmail.com>
2023-06-06 13:58:54 +0200

    Filtro de url

--- routes/routes.php
+++ routes/routes.php
@@ -1,5 +1,28 @@
 <?php
 
+$_SERVER['REQUEST_URI'];
+//explode es como un split
+$routesarray = explode("/",$_SERVER['REQUEST_URI']);
+//limpia las entradasvacías
+$routesarray = array_filter($routesarray);
+
+//imprime el array
+//echo '<pre>'; print_r($routesarray); echo '</pre>';
+
+// Si el array está vacío, devuelve not found
+if(empty($routesarray)){
+
+    $json = array(
+        'status' => 404,
+        'result' => 'Not found'
+    );
+    
+    echo json_encode($json, http_response_code($json["status"]));
+
+    return;
+}
+
+
 $json = array(
     'status' => 200,
     'result' => 'success'
@@ -7,6 +30,6 @@ $json = array(
 
 echo json_encode($json);
 
-return;
+
 
 ?>
